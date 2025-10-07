90c9f95f4567fb5445682749cbe1291ed9ad6f22
Zoee Silcock <mrzoee@gmail.com>
2025-10-07 08:49:16 +0200

    Update stdout/stdin to use the new Reader/Writer interfaces

--- src/repl.zig
+++ src/repl.zig
@@ -8,41 +8,41 @@ const object = @import("object.zig");
 
 const PROMPT = ">> ";
 const MONKEY_FACE =
-\\            __,__
-\\   .--.  .-"     "-.  .--.
-\\  / .. \/  .-. .-.  \/ .. \
-\\ | |  '|  /   Y   \  |'  | |
-\\ | \   \  \ 0 | 0 /  /   / |
-\\  \ '- ,\.-"""""""-./, -' /
-\\   ''-' /_   ^ ^   _\ '-''
-\\       |  \._   _./  |
-\\       \   \ '~' /   /
-\\        '._ '-=-' _.'
-\\           '-----'
+    \\            __,__
+    \\   .--.  .-"     "-.  .--.
+    \\  / .. \/  .-. .-.  \/ .. \
+    \\ | |  '|  /   Y   \  |'  | |
+    \\ | \   \  \ 0 | 0 /  /   / |
+    \\  \ '- ,\.-"""""""-./, -' /
+    \\   ''-' /_   ^ ^   _\ '-''
+    \\       |  \._   _./  |
+    \\       \   \ '~' /   /
+    \\        '._ '-=-' _.'
+    \\           '-----'
 ;
 
-pub fn start(out: std.fs.File, in: std.fs.File) !void {
-    const stdout = out.writer();
-    const stdin = in.reader();
-    var input_buffer: [1024]u8 = undefined;
-
+pub fn start(stdout: *std.Io.Writer, stdin: *std.Io.Reader) !void {
     var permanent_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     var env = try object.Environment.init(permanent_arena.allocator());
     defer env.deinit();
 
-    // try processInput(out, @embedFile("std/array.mnk"), env, &permanent_arena);
+    // try processInput(stdout, @embedFile("std/array.mnk"), env, &permanent_arena);
 
     while (true) {
         _ = try stdout.write(PROMPT);
+        try stdout.flush();
 
-        const input = try stdin.readUntilDelimiter(&input_buffer, '\n');
-        processInput(out, input, env, &permanent_arena) catch continue;
+        const input = try stdin.takeDelimiterInclusive('\n');
+        processInput(stdout, input, env, &permanent_arena) catch continue;
     }
 }
 
-fn processInput(out: std.fs.File, input: []const u8, env: *object.Environment, permanent_arena: *std.heap.ArenaAllocator) !void {
-    const stdout = out.writer();
-
+fn processInput(
+    stdout: *std.Io.Writer,
+    input: []const u8,
+    env: *object.Environment,
+    permanent_arena: *std.heap.ArenaAllocator,
+) !void {
     var temporary_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer temporary_arena.deinit();
 
@@ -53,22 +53,23 @@ fn processInput(out: std.fs.File, input: []const u8, env: *object.Environment, p
     var program = try p.parseProgram();
 
     if (p.errors.len > 0) {
-        try printParserErrors(out, p.errors);
+        try printParserErrors(stdout, p.errors);
     }
 
     if (try evaluator.eval(ast.Node.init(&program), env, permanent_arena.allocator())) |evaluated| {
-        try stdout.print("{s}\n", .{ evaluated.inspect(temporary_arena.allocator()) });
+        try stdout.print("{s}\n", .{evaluated.inspect(temporary_arena.allocator())});
+        try stdout.flush();
     }
 }
 
-fn printParserErrors(out: std.fs.File, errors: []const []const u8) !void {
-    const stdout = out.writer();
-
-    try stdout.print("{s}\n", .{ MONKEY_FACE });
-    try stdout.print("Woops! We ran into some monkey business here!\n", .{ });
-    try stdout.print(" parser errors:\n", .{ });
+fn printParserErrors(stdout: *std.Io.Writer, errors: []const []const u8) !void {
+    try stdout.print("{s}\n", .{MONKEY_FACE});
+    try stdout.print("Woops! We ran into some monkey business here!\n", .{});
+    try stdout.print(" parser errors:\n", .{});
 
     for (errors) |message| {
-        try stdout.print("\t{s}\n", .{ message });
+        try stdout.print("\t{s}\n", .{message});
     }
+
+    try stdout.flush();
 }
